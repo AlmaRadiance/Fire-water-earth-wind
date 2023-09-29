3e8393801bc6a52114794b43f46bb4dd1eafcba1
Alma <codingskills198@gmail.com>
2023-09-29 11:09:16 -0400

    added CSS styles to header and elemental styles, adjusted dropdown styles

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-function computerPlay () {
-    
-}
-const computerPlay = () => {
 
+//create a function that randomly return fire, water, wind, earth
+const computerPlay = () => {
+    const arrOfChoices = ['fire','water','wind','earth']
+    const randomNum = Math.floor (Math.random ()* 4)
+    const compChoice = arrOfChoices [randomNum]
+        return compChoice
+}
+
+//write a function that plays single round of fire, water, wind, earth with 2 parameters 
+//Returns a string that declares the winner & make function playerSelection insensitive 
+//return results of function
+const playRound = (playerSelection, computerSelection) 
+
+//write new function game() 
+//Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+//use loop or call playRound function 5 times in a row
+//use console.log() for results and winner
+//use prompt() to get input from user
+
+const game = () => {
+    for (let i=0; i<5; i++) {
+        const playerSelection = promt ("choose what power element to use: 'fire', 'water','wind','earth'") 
+        console.log(playRound (playerSelection, computerSelection))
+    }
 }
