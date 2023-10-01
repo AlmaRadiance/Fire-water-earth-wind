84945695b1713f0f45bd1a7f7b395e70b59058e5
Alma <codingskills198@gmail.com>
2023-10-01 13:29:37 -0400

    Added event listeners for 'Rules', 'Scores', and 'Round' links to openPopup()

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ const computerPlay = () => {
 //write a function that plays single round of fire, water, wind, earth with 2 parameters 
 //Returns a string that declares the winner & make function playerSelection insensitive 
 //return results of function
-const playRound = (playerSelection, computerSelection) 
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    } else if (
+        (playerSelection === 'fire' && computerSelection === 'wind') ||
+        (playerSelection === 'water' && computerSelection === 'fire') ||
+        (playerSelection === 'wind' && computerSelection === 'water') ||
+        (playerSelection === 'earth' && computerSelection === 'wind')
+    ) {
+        return 'You win this round!';
+    } else {
+        return 'You lose this round!';
+    }
+}
+
 
 //write new function game() 
 //Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
@@ -20,13 +34,14 @@ const playRound = (playerSelection, computerSelection)
 
 const game = () => {
     for (let i=0; i<5; i++) {
-        const playerSelection = promt ("choose what power element to use: 'fire', 'water','wind','earth'") 
-        console.log(playRound (playerSelection, computerSelection))
+        const playerSelection = prompt ("choose what power element to use: 'fire', 'water','wind','earth'");
+        const computerSelection = computerPlay(); 
+        console.log(playRound (playerSelection.toLowerCase(), computerSelection));
     }
 }
 
 
-
+//Opening and closing popup windows
 function openPopup() {
     document.getElementById('popup-container').style.display = 'block';
 }
@@ -34,3 +49,19 @@ function openPopup() {
 function closePopup() {
     document.getElementById('popup-container').style.display = 'none';
 }
+
+
+
+
+// Add an event listener to the "Rules", "scores", "round" link
+document.getElementById('rules-link').addEventListener('click', () => {
+    openPopup(); // Call the openPopup() function when the link is clicked
+});
+
+document.getElementById('scores-link').addEventListener('click', () => {
+    openPopup(); // Call the openPopup() function when the link is clicked
+});
+
+document.getElementById('rounds-link').addEventListener('click', () => {
+    openPopup(); // Call the openPopup() function when the link is clicked
+});
