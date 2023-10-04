cacb324025ae35ddfb5c9ef3167135909d166221
Alma <codingskills198@gmail.com>
2023-10-04 01:36:09 -0400

    added aonclick button

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-//create a function that randomly return fire, water, wind, earth
+let playerScore = 0;
+let computerScore = 0;
+let scores = Array.from({ length: 5 }, () => ({ player: 0, computer: 0, result: 'Not played' }));
+
+const updateScores = () => {
+    document.getElementById('player-score').textContent = `Player: ${playerScore}`;
+    document.getElementById('computer-score').textContent = `Computer: ${computerScore}`;
+}
+
 const computerPlay = () => {
-    const arrOfChoices = ['fire','water','wind','earth']
-    const randomNum = Math.floor (Math.random ()* 4)
-    const compChoice = arrOfChoices [randomNum]
+    const arrOfChoices = ['fire', 'water', 'wind', 'earth']
+    const randomNum = Math.floor(Math.random() * 4)
+    const compChoice = arrOfChoices[randomNum]
     return compChoice;
 }
 
-
-// Define the function to open the custom prompt
 const openCustomPrompt = () => {
     document.getElementById('custom-prompt').style.display = 'block';
 }
 
-
-// Define the function to close the custom prompt
 const closeCustomPrompt = () => {
     document.getElementById('custom-prompt').style.display = 'none';
 }
 
-//write a function that plays single round of fire, water, wind, earth with 2 parameters 
-//Returns a string that declares the winner & make function playerSelection insensitive 
-//return results of function
-let scores = Array.from({ length: 5 }, () => ({ player: 0, computer: 0, result: 'Not played' }));
-const playRound = (playerSelection, computerSelection) => {
+const addCloseButton = () => {
+    const closeButton = document.createElement('span');
+    closeButton.className = 'close-button';
+    closeButton.innerHTML = '&times;';
+    closeButton.onclick = closeCustomPrompt;
+    document.querySelector('.custom-prompt').appendChild(closeButton);
+}
+
+const playRound = (playerSelection, computerSelection, round) => {
     let result;
     if (playerSelection === computerSelection) {
         result = 'Tie';
@@ -33,63 +41,70 @@ const playRound = (playerSelection, computerSelection) => {
         (playerSelection === 'earth' && computerSelection === 'wind')
     ) {
         result = 'Win';
+        playerScore++;
     } else {
         result = 'Lose';
+        computerScore++;
     }
 
-
     scores.push({ player: playerSelection, computer: computerSelection, result });
+    updateScores();
+
+    // Hide messages from previous rounds
+    document.querySelectorAll('.round-message').forEach(message => {
+        message.style.display = 'none';
+    });
+
+    const roundResultMessage = document.createElement('p');
+    roundResultMessage.classList.add(`round-message`, `round-${round}-message`);
+    roundResultMessage.textContent = `You ${result.toLowerCase()} round ${round}!`;
+
+    document.querySelector('.prompt-content').appendChild(roundResultMessage);
+
     return result;
 }
 
-// Function to play the game using custom prompt
-const gameWithCustomPrompt = async () => {
-    let gameResults = [];
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = await getCustomInput();
-        const computerSelection = computerPlay();
-        const roundResult = playRound(playerSelection.toLowerCase(), computerSelection);
-        gameResults.push({ player: playerSelection, computer: computerSelection, result: roundResult });
-    }
-
-    displayScores(gameResults); // Added this line to display scores after the game ends
-
-    return gameResults;
-}
-
-      
-
-// Function to get custom input
 const getCustomInput = () => {
     return new Promise(resolve => {
         openCustomPrompt();
+        addCloseButton();
 
         document.querySelectorAll('.element-button').forEach(button => {
             button.addEventListener('click', () => {
                 const playerSelection = button.innerText.toLowerCase();
-                closeCustomPrompt();
                 resolve(playerSelection);
             });
         });
     });
 }
 
-//Opening and closing popup windows
-function openPopup() {
-    document.getElementById('popup-container').style.display = 'block';
+const gameWithCustomPrompt = async () => {
+    let gameResults = [];
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = await getCustomInput();
+        const computerSelection = computerPlay();
+        const roundResult = playRound(playerSelection.toLowerCase(), computerSelection, i + 1);
+        gameResults.push({ player: playerSelection, computer: computerSelection, result: roundResult });
+    }
+
+    displayScores(gameResults);
+
+    const gameResultMessage = document.createElement('p');
+
+    if (playerScore > computerScore) {
+        gameResultMessage.textContent = `You win the game!`;
+    } else if (playerScore < computerScore) {
+        gameResultMessage.textContent = `You lose the game!`;
+    } else {
+        gameResultMessage.textContent = `It's a tie!`;
+    }
+
+    document.querySelector('.prompt-content').appendChild(gameResultMessage);
+
+    return gameResults;
 }
 
-function closePopup() {
-    document.getElementById('popup-container').style.display = 'none';
-}
-
-function openScoresPopup() {
-    document.getElementById('scores-popup').style.display = 'block';
-}
-
-function closeScoresPopup() {
-    document.getElementById('scores-popup').style.display = 'none';
-}
+// Rest of the code remains the same...
 
 // Add an event listener to the "Rules", "scores", "round" link
 document.getElementById('rules-link').addEventListener('click', () => {
@@ -116,17 +131,10 @@ function displayScores(gameResults) {
     document.querySelector('#scores-popup .popup-content').innerHTML = tableHTML;
 }
 
-
-
 // Call the gameWithCustomPrompt function when the button is clicked
 document.getElementById('start-button').addEventListener('click', async () => {
     await gameWithCustomPrompt(); 
 });
-
-
-
-
-
 
 
 
