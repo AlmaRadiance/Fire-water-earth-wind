0f48d2e1207c1c3d9b3aa6080a23d44d671831f2
Alma <codingskills198@gmail.com>
2023-10-15 17:30:45 -0400

    Translated some existing HTML code into equivalent JavaScript logic

--- index.js
+++ index.js
@@ -1,3 +1,160 @@
+ // Create the header content
+ const header = document.createElement('header');
+ header.classList.add('header');
+ const h1 = document.createElement('h1');
+ h1.innerHTML = ` Welcome to the Elemental Adventure Game: <br> 
+                 <div class="typewriter">
+                     <span class="Rock">Rock</span> -
+                     <span class="Paper">Paper</span> -
+                     <span class="Scissors">Scissors</span> 
+                 </div>`;
+ const dropdown = document.createElement('div');
+ dropdown.classList.add('dropdown');
+ const dropbtn = document.createElement('button');
+ dropbtn.classList.add('dropbtn');
+ dropbtn.innerHTML = '<i class="fa-solid fa-bars"></i> Menu';
+ const dropdownContent = document.createElement('div');
+ dropdownContent.classList.add('dropdown-content');
+ const rulesLink = document.createElement('a');
+ rulesLink.id = 'rules-link';
+ rulesLink.href = '#';
+ rulesLink.innerText = 'Rules';
+ const scoresLink = document.createElement('a');
+ scoresLink.id = 'scores-link';
+ scoresLink.href = '#';
+ scoresLink.innerText = 'Scores';
+ header.appendChild(h1);
+ dropdownContent.appendChild(rulesLink);
+ dropdownContent.appendChild(scoresLink);
+ dropdown.appendChild(dropbtn);
+ dropdown.appendChild(dropdownContent);
+ header.appendChild(dropdown);
+ 
+ // Create the start button content
+ const startButtonDiv = document.createElement('div');
+ startButtonDiv.classList.add('start-button');
+ const startButton = document.createElement('button');
+ startButton.classList.add('main-button');
+ startButton.id = 'start-button';
+ startButton.innerHTML = `<i class="material-icons-outlined">sports_esports</i> PLAY`;
+ startButtonDiv.appendChild(startButton);
+
+
+
+
+
+ // Create the footer content
+const footer = document.createElement('footer');
+footer.classList.add('footer');
+footer.innerHTML = '&copy; 2023 ROCK-PAPER-SCISSORS - THE ODIN PROJECT | BY: ALMA P.';
+
+
+
+
+
+ // Create the popup container for game rules
+ const gameRulesPopup = document.createElement('div');
+ gameRulesPopup.id = 'popup-container';
+ gameRulesPopup.classList.add('popup');
+ const gameRulesPopupContent = document.createElement('div');
+ gameRulesPopupContent.classList.add('popup-content');
+ const closeGameRulesPopup = document.createElement('span');
+ closeGameRulesPopup.classList.add('close-popup');
+ closeGameRulesPopup.setAttribute('onclick', 'closePopup()');
+ closeGameRulesPopup.innerHTML = '&times;';
+ gameRulesPopupContent.appendChild(closeGameRulesPopup);
+ const gameRulesHeading = document.createElement('h2');
+ gameRulesHeading.innerText = 'Game Rules';
+ const gameRulesText = document.createElement('p');
+ gameRulesText.style.fontWeight = 'normal';
+ gameRulesText.innerHTML = `Welcome to the Rock-Paper-Scissors Game!
+                          <h3>Rules</h3>
+                          Get ready for a classic game of Rock-Paper-Scissors against the computer!
+                          <ul>
+                              <li>Each player selects either Rock, Paper, or Scissors.</li>
+                              <li>The computer will also randomly choose one of the options.</li>
+                              <li>The winner is determined based on the following rules:
+                                  <ul>
+                                      <li>Rock beats Scissors</li>
+                                      <li>Scissors beats Paper</li>
+                                      <li>Paper beats Rock</li>
+                                  </ul>
+                              </li>
+                              <li>Prepare for an epic contest spanning five rounds. Every selection, every move, will determine the course of the battle.</li>
+                              <li>With every round, the stage is set for the ultimate victor. Choose your move wisely, for in this game, only the sharpest will triumph!</li>
+                          </ul>`;
+ const gameRulesChooseMoveHeading = document.createElement('h3');
+ gameRulesChooseMoveHeading.innerText = 'Choose Your Move';
+ const gameRulesElements = document.createElement('div');
+ gameRulesElements.classList.add('elements');
+ const gameRulesElementsList = document.createElement('ul');
+ const rockElement = document.createElement('li');
+ rockElement.innerHTML = `<img class="img-bounce" src="./Images/r.jpg" alt="Rock" width="140" height="80">
+                          <p>Rock</p>
+                          <p style="font-weight: normal;">Solid and unyielding, Rock crushes Scissors but is covered by Paper.</p>`;
+ const paperElement = document.createElement('li');
+ paperElement.innerHTML = `<img class="img-bounce" src="./Images/p.jpg" alt="Paper" width="140" height="80">
+                           <p>Paper</p>
+                           <p style="font-weight: normal;">Thin and protective, Paper covers Rock but is cut by Scissors.</p>`;
+ const scissorsElement = document.createElement('li');
+ scissorsElement.innerHTML = `<img class="img-bounce" src="./Images/s.jpg" alt="Scissors" width="140" height="80">
+                              <p>Scissors</p>
+                              <p style="font-weight: normal;">Sharp and precise, Scissors cut Paper but are crushed by Rock.</p>`;
+ gameRulesElementsList.appendChild(rockElement);
+ gameRulesElementsList.appendChild(paperElement);
+ gameRulesElementsList.appendChild(scissorsElement);
+ gameRulesElements.appendChild(gameRulesElementsList);
+ 
+ // Create the scores popup container
+ const scoresPopup = document.createElement('div');
+ scoresPopup.id = 'scores-popup';
+ scoresPopup.classList.add('popup');
+ const scoresPopupContent = document.createElement('div');
+ scoresPopupContent.classList.add('popup-content');
+ const closeScoresPopupButton = document.createElement('button');
+ closeScoresPopupButton.classList.add('close-popup-button');
+ closeScoresPopupButton.setAttribute('onclick', 'closeScoresPopup()');
+ closeScoresPopupButton.innerText = 'Close';
+ scoresPopupContent.appendChild(closeScoresPopupButton);
+ 
+ // Append elements to the body
+ document.body.appendChild(header);
+ document.body.appendChild(startButtonDiv);
+ document.body.appendChild(gameRulesPopup);
+ gameRulesPopup.appendChild(gameRulesPopupContent);
+ gameRulesPopupContent.appendChild(gameRulesHeading);
+ gameRulesPopupContent.appendChild(gameRulesText);
+ gameRulesPopupContent.appendChild(gameRulesChooseMoveHeading);
+ gameRulesPopupContent.appendChild(gameRulesElements);
+ document.body.appendChild(scoresPopup);
+ scoresPopup.appendChild(scoresPopupContent);
+ document.body.appendChild(footer);
+
+ 
+ // Add event listeners
+ document.getElementById('rules-link').addEventListener('click', () => {
+     openPopup();
+ });
+ 
+ document.getElementById('scores-link').addEventListener('click', () => {
+     openScoresPopup();
+ });
+ 
+ document.getElementById('start-button').addEventListener('click', async () => {
+     await gameWithCustomPrompt();
+ });
+ 
+ // Define the closePopup and closeScoresPopup functions
+ function closePopup() {
+     document.getElementById('popup-container').style.display = 'none';
+ }
+ 
+ function closeScoresPopup() {
+     document.getElementById('scores-popup').style.display = 'none';
+ }
+
+
+
 let playerScore = 0;
 let computerScore = 0;
 let scores = Array.from({ length: 5 }, () => ({ player: 'Not played', computer: 'Not played', result: 'Not played' }));
@@ -166,7 +323,7 @@ function closeScoresPopup() {
 
 
 
-// Add an event listener to the "Rules", "scores", "round" link
+// Add an event listener to the "Rules", "scores" link
 document.getElementById('rules-link').addEventListener('click', () => {
     openPopup(); // Call the openPopup() function when the link is clicked
 });
